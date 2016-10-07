acc6ba095df30ad1afd826c0e855fd0278b3de7e
Erwin de Haan <EraYaN@users.noreply.github.com>
2016-10-07 15:01:53 +0200

    Updated model.

--- src/compile.ps1
+++ src/compile.ps1
@@ -2,9 +2,10 @@
 # 2016-09-23
 # v0.1
 param (
-    [string]$project = "test",
-    [string]$trace_action = "",
-    [switch]$show_graph = $false
+    [string]$Project = "test",
+    [string]$TraceAction = "",
+    [switch]$ShowGraph = $false,
+	[switch]$CleanupOnly = $false
 )
 
 $mCRL2RegData = Get-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\mCRL2
@@ -13,29 +14,33 @@ Write-Host "mCRL2 installed in: " $mCRL2Path -ForegroundColor Yellow
 $env:Path += ";$mCRL2Path"
 
 Write-Host "Cleaning Up" -ForegroundColor Green
-Remove-Item "${project}.lps*"
-Remove-Item "${project}.lts"
-Write-Host "Compiling" -ForegroundColor Green
-cmd /C mcrl22lps.exe "${project}.mcrl2" "${project}.lps" --lin-method=regular --rewriter=jitty
-cmd /C lps2lts.exe "${project}.lps" "${project}.lts" --rewriter=jitty --strategy=breadth
+Remove-Item "${Project}.lps*"
+Remove-Item "${Project}.lts"
+if(!$CleanupOnly){
+	Write-Host "Compiling LPS" -ForegroundColor Green
+	cmd /C mcrl22lps.exe "${Project}.mcrl2" "${Project}.lps" --lin-method=regular --rewriter=jitty
+	Write-Host "Compiling LTS" -ForegroundColor Green
+	cmd /C lps2lts.exe "${Project}.lps" "${Project}.lts" --rewriter=jitty --strategy=breadth
 
-if($trace_action){
-	Write-Host "Generating Traces" -ForegroundColor Green
-	cmd /C lps2lts.exe  "${project}.lps" "${project}_trace_${trace_action}.lts" --action=$trace_action --rewriter=jitty --strategy=breadth --trace
-	$traces = Get-ChildItem "${project}.lps*.trc"
-	ForEach ($trace In $traces) {
-		cmd /C tracepp "$trace" "${trace}.txt" --format=plain
+	if($TraceAction){
+		Write-Host "Generating Traces" -ForegroundColor Green
+		cmd /C lps2lts.exe  "${Project}.lps" "${Project}_trace_${TraceAction}.lts" --action=$TraceAction --rewriter=jitty --strategy=breadth --trace
+		$traces = Get-ChildItem "${Project}.lps*.trc"
+		ForEach ($trace In $traces) {
+			cmd /C tracepp "$trace" "${trace}.txt" --format=plain
+		}
+		$traces_results = Get-ChildItem "${Project}.lps*.trc.txt"
+		$i = 0;
+		ForEach ($trace_result In $traces_results) {
+			Write-Host "Trace ${i}:"
+			Get-Content "$trace_result"
+			$i++;
+		}
 	}
-	$traces_results = Get-ChildItem "${project}.lps*.trc.txt"
-	$i = 0;
-	ForEach ($trace_result In $traces_results) {
-		Write-Host "Trace ${i}:"
-		Get-Content "$trace_result"
-		$i++;
+	Write-Host "Generation Done." -ForegroundColor Green
+	if($ShowGraph){
+		Write-Host "Launching LTSGraph" -ForegroundColor Green
+		cmd /C START ltsgraph.exe "${Project}.lts"
 	}
 }
-
-if($show_graph){
-	Write-Host "Launching LTSGraph" -ForegroundColor Green
-	cmd /C START ltsgraph.exe "${project}.lts"
-}
+Write-Host "Done." -ForegroundColor Green
